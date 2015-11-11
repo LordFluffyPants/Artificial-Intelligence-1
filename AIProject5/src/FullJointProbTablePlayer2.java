1538ce738aa004e924d522f6270a909ef6bd874e
lordfluffypants <rixmaddi@gmail.com>
2015-11-11 02:33:55 -0500

    49% to 51% win ratio using next move and move effect

--- AIProject5/src/FullJointProbTablePlayer2.java
+++ AIProject5/src/FullJointProbTablePlayer2.java
@@ -7,11 +7,10 @@ import java.util.List;
  * Created by jakehayhurst on 11/9/15.
  */
 public class FullJointProbTablePlayer2 extends NannonPlayer{
-
     private int [][][][][][] winning_board_states = new int[3][3][3][3][3][3];
     private int [][][][][][] losing_board_states = new int[3][3][3][3][3][3];
-    private int [] winning_effect_moves = new int[ManageMoveEffects.COUNT_OF_MOVE_DESCRIPTORS];
-    private int [] losing_effect_moves = new int[ManageMoveEffects.COUNT_OF_MOVE_DESCRIPTORS];
+    private int [][][][][][][] winning_board_next_states = new int[3][3][3][3][3][3][12];
+    private int [][][][][][][] losing_board_next_states = new int[3][3][3][3][3][3][12];
     private int [][] winning_moves = new int[7][7];
     private int [][] losing_moves = new int[7][7];
     private  int number_of_wins = 1;
@@ -25,6 +24,22 @@ public class FullJointProbTablePlayer2 extends NannonPlayer{
     }
 
     private void initialize() {
+        for (int [][][][][][] step_two : winning_board_next_states)
+            for (int [][][][][] step_three : step_two)
+                for (int [][][][] step_four : step_three)
+                    for (int [][][] step_five : step_four)
+                        for (int [][] step_six : step_five)
+                            for(int [] step_seven : step_six)
+                                Arrays.fill(step_seven,1);
+
+        for (int [][][][][][] step_two : losing_board_next_states)
+            for (int [][][][][] step_three : step_two)
+                for (int [][][][] step_four : step_three)
+                    for (int [][][] step_five : step_four)
+                        for (int [][] step_six : step_five)
+                            for (int [] step_seven : step_six)
+                                Arrays.fill(step_seven,1);
+
         for (int [][][][][] step_two : winning_board_states)
             for (int [][][][] step_three : step_two)
                 for (int [][][] step_four : step_three)
@@ -32,11 +47,12 @@ public class FullJointProbTablePlayer2 extends NannonPlayer{
                         for (int [] step_six : step_five)
                             Arrays.fill(step_six,1);
 
-        for (int i = 0; i < ManageMoveEffects.COUNT_OF_MOVE_DESCRIPTORS; i++)
-        {
-            winning_effect_moves[i] = 1;
-            losing_effect_moves[i] = 1;
-        }
+        for (int [][][][][] step_two : losing_board_states)
+            for (int [][][][] step_three : step_two)
+                for (int [][][] step_four : step_three)
+                    for (int [][] step_five : step_four)
+                        for (int [] step_six : step_five)
+                            Arrays.fill(step_six,1);
         for (int [] steps : winning_moves)
         {
             Arrays.fill(steps,1);
@@ -70,17 +86,18 @@ public class FullJointProbTablePlayer2 extends NannonPlayer{
 
                 int[] resultingBoard = gameBoard.getNextBoardConfiguration(boardConfiguration, move);
 
-
-                double next_state_given_win_prob =  (double) winning_board_states[resultingBoard[7]][resultingBoard[8]][resultingBoard[9]][resultingBoard[10]][resultingBoard[11]][resultingBoard[12]] /(double)number_of_wins;
-                double effect_prob_given_win = (double) winning_effect_moves[effect] / (double)number_of_wins;
-                double effect_prob_give_loss = (double) losing_effect_moves[effect] / (double)number_of_losses;
-                double win_prob = (double)number_of_wins / ((double)number_of_wins + (double)number_of_losses);
-                double next_state_given_loss_prob = (double) losing_board_states[resultingBoard[7]][resultingBoard[8]][resultingBoard[9]][resultingBoard[10]][resultingBoard[11]][resultingBoard[12]] / (double)number_of_losses;
-                double loss_prob = (double)number_of_losses / ((double)number_of_wins + (double)number_of_losses);
+                double current_state_given_win = (double) winning_board_states[boardConfiguration[7]][boardConfiguration[8]][boardConfiguration[9]][boardConfiguration[10]][boardConfiguration[11]][boardConfiguration[12]] /(double)number_of_wins;
+                double next_state_and_move_effect_given_win_prob =  (double) winning_board_next_states[resultingBoard[7]][resultingBoard[8]][resultingBoard[9]][resultingBoard[10]][resultingBoard[11]][resultingBoard[12]][effect] /(double)number_of_wins;
                 double move_prob_given_win = (double)winning_moves[from][to] / (double)number_of_wins;
-                double move_prob_given_loss = (double)losing_moves[from][to] / (double)number_of_losses;
+                double win_prob = (double)number_of_wins / ((double)number_of_wins + (double)number_of_losses);
 
-                double ratio_of_success= (next_state_given_win_prob* effect_prob_given_win * move_prob_given_win* win_prob) / (next_state_given_loss_prob * effect_prob_give_loss  * move_prob_given_loss* loss_prob);
+                double current_state_given_loss = (double) losing_board_states[boardConfiguration[7]][boardConfiguration[8]][boardConfiguration[9]][boardConfiguration[10]][boardConfiguration[11]][boardConfiguration[12]] /(double)number_of_losses;
+                double next_state_and_move_effect_given_loss_prob = (double) losing_board_next_states[resultingBoard[7]][resultingBoard[8]][resultingBoard[9]][resultingBoard[10]][resultingBoard[11]][resultingBoard[12]][effect] / (double)number_of_losses;
+                double move_prob_given_loss = (double)losing_moves[from][to] / (double)number_of_losses;
+                double loss_prob = (double)number_of_losses / ((double)number_of_wins + (double)number_of_losses);
+
+                double ratio_of_success=    ( next_state_and_move_effect_given_win_prob * win_prob) /
+                                            ( next_state_and_move_effect_given_loss_prob * loss_prob);
 
                 if (ratio_of_success > highest_ratio) {
                     highest_ratio = ratio_of_success;
@@ -119,14 +136,14 @@ public class FullJointProbTablePlayer2 extends NannonPlayer{
 
             if (didIwinThisGame)
             {
-                winning_board_states[resultingBoard[7]][resultingBoard[8]][resultingBoard[9]][resultingBoard[10]][resultingBoard[11]][resultingBoard[12]]++;
-                winning_effect_moves[effect]++;
+                winning_board_states[currentBoard[7]][currentBoard[8]][currentBoard[9]][currentBoard[10]][currentBoard[11]][currentBoard[12]]++;
+                winning_board_next_states[resultingBoard[7]][resultingBoard[8]][resultingBoard[9]][resultingBoard[10]][resultingBoard[11]][resultingBoard[12]][effect]++;
                 winning_moves[from][to]++;
             }
             else
             {
-                losing_board_states[resultingBoard[7]][resultingBoard[8]][resultingBoard[9]][resultingBoard[10]][resultingBoard[11]][resultingBoard[12]]++;
-                losing_effect_moves[effect]++;
+                losing_board_states[currentBoard[7]][currentBoard[8]][currentBoard[9]][currentBoard[10]][currentBoard[11]][currentBoard[12]]++;
+                losing_board_next_states[resultingBoard[7]][resultingBoard[8]][resultingBoard[9]][resultingBoard[10]][resultingBoard[11]][resultingBoard[12]][effect]++;
                 losing_moves[from][to]++;
             }
         }
